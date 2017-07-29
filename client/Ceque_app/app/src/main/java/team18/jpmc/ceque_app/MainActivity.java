74704a9676f2409735b20639d61699b7af586cfa
zbhargav15 <zbhargav15@gmail.com>
2017-07-29 17:57:55 +0530

    Upload button starts recording video using intent

--- client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/MainActivity.java
+++ client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/MainActivity.java
@@ -1,13 +1,51 @@
 package team18.c4g.ceque_app;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_VIDEO_CAPTURE = 1;
 
+    VideoView videoresult;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b1 = (Button) findViewById(R.id.upload);
+
+        videoresult = (VideoView) findViewById(R.id.videoupload);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                videoviewer();
+            }
+    });
+    }
+
+
+public void videoviewer(){
+        {
+            Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            if (takeVideoIntent.resolveActivity(this.getPackageManager()) != null) {
+                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+            }
+        }
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            videoresult.setVideoURI(videoUri);
+            videoresult.start();
+
+        }
     }
 }
