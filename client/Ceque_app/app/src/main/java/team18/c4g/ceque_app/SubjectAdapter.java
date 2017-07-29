62b1c8e066eb1466633068cfb0891185b41605f9
jayeshsaita <jsaita96@gmail.com>
2017-07-29 23:06:17 +0530

    Testing subject filter

---
+++ client/Ceque_app/app/src/main/java/team18/c4g/ceque_app/SubjectAdapter.java
@@ -0,0 +1,51 @@
+package team18.c4g.ceque_app;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import team18.c4g.ceque_app.retrofit_model.AllSubject;
+
+/
+ * Created by Soundwave on 29-Jul-17.
+ */
+
+public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHolder> {
+
+    List<AllSubject> subjectList;
+
+    public SubjectAdapter(List<AllSubject> subjectList){
+        this.subjectList = subjectList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_item,parent, false);
+
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.text.setText(subjectList.get(position).getAllSubjects().get(position).getSubName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return subjectList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView text;
+        public ViewHolder(View itemView) {
+            super(itemView);
+             text = (TextView)itemView.findViewById(R.id.item);
+        }
+    }
+}
---
+++ client/Ceque_app/app/src/main/java/team18/c4g/ceque_app/retrofit_model/Subjects.java
@@ -0,0 +1,34 @@
+package team18.c4g.ceque_app.retrofit_model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/
+ * Created by Soundwave on 29-Jul-17.
+ */
+
+public class Subjects {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("sub_name")
+    @Expose
+    private String subName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+}
