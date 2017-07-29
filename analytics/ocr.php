9ce20048adace2cc20477e27bae1b0b91fe6c1e7
SomeUser <ashok05@somaiya.edu>
2017-07-30 02:24:08 +0530

    Added PHP OCR retriever

---
+++ analytics/ocr.php
@@ -0,0 +1,22 @@
+<?php
+ 
+$url = 'http://www.bitocr.com/api';
+$user_url = 'http://www.bitocr.com/images/ocrtest.jpg'
+$fields = array('url' => '$user_url', 'apikey' => '184108e25758d07e', 'lang' => 'eng');
+$resource = curl_init();
+curl_setopt($resource, CURLOPT_URL, $url);
+curl_setopt($resource, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($resource, CURLOPT_POST, 1);
+curl_setopt($resource, CURLOPT_POSTFIELDS, $fields);
+$result = json_decode(curl_exec($resource),true);
+curl_close($resource);
+ 
+if($result['error']==0){
+    //success
+    echo($result['result']);		
+}else{
+    //failed
+    echo("Error #" . $result['error_code'] . " " . $result['error_message']);	
+}
+ 
+?>
