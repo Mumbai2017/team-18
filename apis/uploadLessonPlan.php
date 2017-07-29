692f0f7e7b10d60e258c8cd69a683933019151eb
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2017-07-29 18:18:48 +0530

    Add apis here

--- apis/uploadLessonPlan.php
+++ apis/uploadLessonPlan.php
@@ -1,8 +1,17 @@
 <?php
 	include 'conn.php';
 	$t=time();
+	
 	if(isset($_POST["image_url"])){
-		$updateImageOfLessonPlan = mysqli_query($conn,"INSERT INTO `lesson_plan` (`image_url`,`timestamp`) VALUES ('".$_POST["image_url"]."',".$t.")");
+		
+		$image = $_POST["image_url"];
+		$title = array_merge(range('a', 'z'), range('A', 'Z'));
+		shuffle($title);
+		$title = substr(implode($title), 0, 6);
+		$upload_path = "../uploads/".$title.".jpg";
+		$image_path = "http://cfg.hphost.in/uploads/".$title.".jpg";
+		file_put_contents($upload_path, base64_decode($image));
+		$updateImageOfLessonPlan = mysqli_query($conn,"INSERT INTO `lesson_plan` (`image_url`,`timestamp`) VALUES ('".$upload_path."',".$t.")");
 		echo "true";
 	}
 	else{
