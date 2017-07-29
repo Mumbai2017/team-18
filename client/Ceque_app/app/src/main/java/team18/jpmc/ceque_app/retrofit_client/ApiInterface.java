5e33e779d0320de89ad96c5c1abb1be4a6e9ddb2
jayeshsaita <jsaita96@gmail.com>
2017-07-29 23:06:17 +0530

    Testing subject filter

---
+++ client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/retrofit_client/ApiInterface.java
@@ -0,0 +1,18 @@
+package team18..ceque_app.retrofit_client;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import team18..ceque_app.retrofit_model.AllSubject;
+
+/
+ * Created by Soundwave on 29-Jul-17.
+ */
+
+public interface ApiInterface {
+
+    @GET("getAllSubjects.php")
+    Call<List<AllSubject>> getAllSubjects();
+
+}
