f7776624b75efb11e5e06b0e42ac9b0ac29fea7e
zbhargav15 <zbhargav15@gmail.com>
2017-07-29 21:24:14 +0530

    trying volley to implement spinners

--- client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/MainActivity.java
+++ client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/MainActivity.java
@@ -5,13 +5,28 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.Spinner;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.R.attr.bitmap;
 import static android.R.attr.data;
@@ -32,7 +47,12 @@ public class MainActivity extends AppCompatActivity {
         Spinner s3=(Spinner) findViewById(R.id.topicspin);
         videoresult = (VideoView) findViewById(R.id.videoupload);
         mediaC= new MediaController(this);
-
+        s1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                fetchData1();
+            }
+        });
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -85,5 +105,27 @@ public void videoviewer(){
         }
 
     }
+public void fetchData1(){
+    RequestQueue queue = Volley.newRequestQueue(this); // this = context
+    String url = "http://cfg.hphost.in/apis/getAllSubjects.php";
+    StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                   Log.d("today",response);
 
+
+
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // error
+                    Log.d("Error.Response", "error");
+                }
+            }
+    );
+
+}
     }
