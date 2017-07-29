2020d30026c4f7b15976434557b907a821d35867
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2017-07-29 21:40:47 +0530

    added apis for upload image, get lesson plans, subject, topic, units, particular plan and seek values

--- apis/uploadLessonPlan.php
+++ apis/uploadLessonPlan.php
@@ -1,9 +1,9 @@
 <?php
-	include 'conn.php';
+	/*include 'conn.php';
 	$t=time();
 	
 	if(isset($_POST["image_url"])){
-		
+
 		$image = $_POST["image_url"];
 		$title = array_merge(range('a', 'z'), range('A', 'Z'));
 		shuffle($title);
@@ -16,5 +16,39 @@
 	}
 	else{
 		echo "false";
+	}*/
+?>
+<?php
+ header('Content-type : bitmap; charset=utf-8');
+ 
+ if(isset($_POST["encoded_string"])){
+ 	
+	$encoded_string = $_POST["encoded_string"];
+	$image_name = $_POST["image_name"];
+	
+	$decoded_string = base64_decode($encoded_string);
+	
+	$path = 'images/'.$image_name;
+	
+	$file = fopen($path, 'wb');
+	
+	$is_written = fwrite($file, $decoded_string);
+	fclose($file);
+	
+	if($is_written > 0) {
+		
+		$connection = mysqli_connect('185.120.7.1', 'cfghphost', 'qB9vm{kyMcpr','cfghphos_main');
+		$query = "INSERT INTO lesson_plan(image_url,timestamp) values('$image_name','$path');";
+		
+		$result = mysqli_query($connection, $query) ;
+		
+		if($result){
+			echo "success";
+		}else{
+			echo "failed";
+		}
+		
+		mysqli_close($connection);
 	}
+ }
 ?>
