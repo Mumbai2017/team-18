4f7f19e38614d777ea1fbc74893649c20e8a245c
nikhilvatwani <nikhil.gymtrekker@gmail.com>
2017-07-29 17:00:30 +0530

    added upload image api

--- apis/uploadLessonPlan.php
+++ apis/uploadLessonPlan.php
@@ -1,7 +1,8 @@
 <?php
 	include 'conn.php';
+	$t=time();
 	if(isset($_POST["image_url"])){
-		$updateImageOfLessonPlan = mysqli_query($conn,"INSERT INTO `lesson_plan` (`image_url`) VALUES ('".$_POST["image_url"]."')");
+		$updateImageOfLessonPlan = mysqli_query($conn,"INSERT INTO `lesson_plan` (`image_url`,`timestamp`) VALUES ('".$_POST["image_url"]."',".$t.")");
 		echo "true";
 	}
 	else{
