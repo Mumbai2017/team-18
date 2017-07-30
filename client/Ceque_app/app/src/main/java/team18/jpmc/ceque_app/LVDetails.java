96915fc596c5d785212a9fa90a9a048685cddd3e
twinkledhanak <twinkledhanak@gmail.com>
2017-07-30 06:30:05 +0530

    Minor fixes for video details
    Minor fixes for video details

--- client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/LVDetails.java
+++ client/Ceque_app/app/src/main/java/team18/jpmc/ceque_app/LVDetails.java
@@ -18,14 +18,17 @@ import retrofit.client.Response;
 
 public class LVDetails extends AppCompatActivity {
 
-    SharedPreferences sharedPreferences ;
+    SharedPreferences sharedPreferences;
     public static final String ROOT_URL = "http://cfg.hphost.in/apis/";
     private List<LessonPlan> person;
+    private List<Video> person2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lvdetails);
         getUser();
+        getVideo();
     }
 
 
@@ -56,33 +59,92 @@ public class LVDetails extends AppCompatActivity {
 
     }
 
-    private void showList()
-    {
+    private void getVideo() {
+        //Creating a rest adapter
+        RestAdapter adapter = new RestAdapter.Builder()
+                .setEndpoint(ROOT_URL)
+                .build();
+
+        //Creating an object of our api interface
+        GetApiVideo api = adapter.create(GetApiVideo.class);
+
+        //Defining the method
+        api.getVideo(new Callback<List<Video>>() {
+
+            @Override
+            public void success(List<Video> lessonPlen, Response response) {
+
+                person2 = lessonPlen;
+                showList2();
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+    }
+
+    private void showList() {
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-       String id = sharedPreferences.getString("id","0");
+        String id = sharedPreferences.getString("id", "0");
+
         int pos = 0, ctr = 0;
+
         String[] items = new String[person.size()];
-        for(int i=0; i<person.size(); i++)
-        {
-            if(Integer.parseInt(id)==i) {
+        for (int i = 0; i < person.size(); i++) {
+            if (Integer.parseInt(id) == i) {
                 items[i] = String.valueOf(person.get(i).getId());
                 pos = i;
                 break;
             }
 
         }
-        Button b1 = (Button)findViewById(R.id.b1);
-        b1.setText("L E S S O N - P L A N   F O R :" +String.valueOf(person.get(pos).getId()) );
+        Button b1 = (Button) findViewById(R.id.b1);
+        b1.setText("L E S S O N - P L A N   F O R :" + String.valueOf(person.get(pos).getId()));
 
 
-        TextView details = (TextView)findViewById(R.id.detail);
+        TextView details = (TextView) findViewById(R.id.detail);
         details.setText(
-                "USER:"+ String.valueOf(person.get(pos).getUser_id())+ "\n" +
-                        "TOPIC: "+String.valueOf(person.get(pos).getTopic_id())+ "\n" +
-                        "DESCRIPTION: "+String.valueOf(person.get(pos).getDescription())+ "\n" +
-                        "TIME-IN-MILLIS: "+String.valueOf(person.get(pos).getTimestamp())+ "\n" +
-                        "IMAGE-URL: "+  String.valueOf(person.get(pos).getImageurl())
+                "USER:" + String.valueOf(person.get(pos).getUser_id()) + "\n" +
+                        "TOPIC: " + String.valueOf(person.get(pos).getTopic_id()) + "\n" +
+                        "DESCRIPTION: " + String.valueOf(person.get(pos).getDescription()) + "\n" +
+                        "TIME-IN-MILLIS: " + String.valueOf(person.get(pos).getTimestamp()) + "\n" +
+                        "IMAGE-URL: " + String.valueOf(person.get(pos).getImageurl())
         );
+    }
+
+
+
+        private void showList2()
+        {
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            String id = sharedPreferences.getString("id", "0");
+            int pos = 0;
+            String[] items = new String[person2.size()];
+            for (int i = 0; i < person2.size(); i++) {
+                if (Integer.parseInt(id) == i) {
+                    items[i] = String.valueOf(person2.get(i).getId());
+                    pos = i;
+                    break;
+                }
+
+            }
+            Button b2 = (Button) findViewById(R.id.b2);
+            b2.setText("V I D E O   F O R :" + String.valueOf(person2.get(pos).getId()));
+
+
+            TextView details2 = (TextView) findViewById(R.id.detail);
+            details2.setText(
+                    "USER:" + String.valueOf(person2.get(pos).getUser_id()) + "\n" +
+                            "YOUTUBE ID : " + String.valueOf(person2.get(pos).getYoutube_id()) + "\n" +
+                            "IP ID: " + String.valueOf(person2.get(pos).getIp_id()) + "\n" +
+                            "TIME-IN-MILLIS: " + String.valueOf(person2.get(pos).getTimestamp()) + "\n"
+
+            );
+
+        }
 
 
 
@@ -92,4 +154,4 @@ public class LVDetails extends AppCompatActivity {
 
 
 
-}
+
